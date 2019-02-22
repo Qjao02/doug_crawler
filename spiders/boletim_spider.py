4845f07b80239983d67650186fe06724da4bc550
qjao02 <joao_Cruuz02@hotmail.com>
2019-02-22 17:07:26 -0300

    changed crawler strategy

--- spiders/boletim_spider.py
+++ spiders/boletim_spider.py
@@ -9,12 +9,12 @@ class BoletimSpider(scrapy.Spider):
             urls.append('https://ufsj.edu.br/ascom/boletim{0:03}.php'.format(i))
 
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse, encoding='utf-8')
 
     def parse(self, response):
         boletim =  {
-            'title' :  response.xpath('//p[@class="titulo-int2"]/text()').get().encode('utf-8'),
-            'news' :  response.xpath('//div/p/strong/text()').getall().encode('utf-8')
+            'title' :  response.xpath('//p[@class="titulo-int2"]/text()').get(),
+            'news' :  response.xpath('//div/p/strong/text()').getall()
         }
 
         return boletim
